f8ef0879b56f14ae55b30e2ee34cc6c622c2d036
oskar <oskar_ruyter@hotmail.com>
2020-01-23 15:16:05 +0100

    Hei

---
+++ Tests/sjekkLoggInnTest.php
@@ -0,0 +1,73 @@
+<?php
+include_once '../Model/domeneModell.php';
+include_once '../BLL/bankLogikk.php';
+
+
+
+class sjekkLoggInnTest extends PHPUnit\Framework\TestCase {
+
+    public function testRiktigPersonnummer() {
+        // arrange
+        $personnummer = "12345678910";
+        $personnummer2 = "11119743783";
+        // act
+
+        // assertion
+        $this->assertTrue((boolean)preg_match("/^[0-9]{11}$/", $personnummer));
+        $this->assertTrue((boolean)preg_match("/^[0-9]{11}$/", $personnummer2));
+    }
+
+    public function testFeilPersonnummer() {
+        // arrange
+        $personnummer = "fjwfh,welfe";
+        $personnummer2 = "3456788";
+        $personnummer3 = "#¤%&/((/";
+        // act
+
+        // assertion
+        $this->assertFalse((boolean)preg_match("/^[0-9]{11}$/", $personnummer));
+        $this->assertFalse((boolean)preg_match("/^[0-9]{11}$/", $personnummer2));
+        $this->assertFalse((boolean)preg_match("/^[0-9]{11}$/", $personnummer3));
+    }
+
+    public function testRiktigPassord() {
+        //arrange
+        $passord = "HeiHei";
+        $passord2 = "oiiJMNeje_dd.ff35";
+        $passord3 = "cbbwfwe567765--HDHJD";
+
+        // assertion
+        $this->assertTrue((boolean)preg_match("/^.{6,30}$/", $passord));
+        $this->assertTrue((boolean)preg_match("/^.{6,30}$/", $passord2));
+        $this->assertTrue((boolean)preg_match("/^.{6,30}$/", $passord3));
+    }
+
+    public function testFeilPassord() {
+        //arrange
+        $passord = "hei";
+        $passord2 = "1234";
+        $passord3 = "heiggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg";
+
+        // assertion
+        $this->assertFalse((boolean)preg_match("/^.{6,30}$/", $passord));
+        $this->assertFalse((boolean)preg_match("/^.{6,30}$/", $passord2));
+        $this->assertFalse((boolean)preg_match("/^.{6,30}$/", $passord3));
+    }
+
+    public function testVellykketLoggInn() {
+        // arrange
+        $personnummer = "12345678910";
+        $passord = "HeiHei";
+        $bank = new Bank(new BankDBStub());
+        // act
+        $ok = $bank->sjekkLoggInn($personnummer, $passord);
+        // assertion
+        $this->assertTrue($ok);
+
+
+
+
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'View/minNavigasjon.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<br/><br/>
+<div class="container">
+    <br/><br/>
+    <h4>Velkommen til TestingBank</h4>
+    <p>Trykk her for å logge inn</p>
+    <br/>
+    <a href="View/loggInn.php" class="btn btn-success">Logg inn</a>
+    <br/> <br/>
+    <div id="feilMeldinger"></div>
+</div>
+</body>
+</html>
+
+
